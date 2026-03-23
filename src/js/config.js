fcd6ff8e9b10d47bc02b2d0b22b914bd89861add
Noah Clark <Clarknoah@live.com>
2026-03-23 17:00:38 -0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '66f61b04-bc18-4999-abcf-d1f81dcb2cf2';
+export const MODAL_CLOSE_SEC = 2.5;
